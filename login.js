04a7462210db9562e85d3f14324c1fce5b5b5f6d
Dhananjay Mishra <jaymishra2005chandni@gmail.com>
2025-12-26 10:32:13 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,6 +3,9 @@ const bodyparser = require('body-parser');
 const path = require('path');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req,res) => {
